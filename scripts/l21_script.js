9b5b754a3d0b34f99cba06d51dca12dda075a9b8
InsciusZZO <insciuszzo@gmail.com>
2025-02-18 16:39:32 +0200

    sorted all files into folders, also added 21 js hw

---
+++ scripts/l21_script.js
@@ -0,0 +1,10 @@
+let country = "Moldova";
+let continent = "Europe";
+let population = 2.5;
+let isIsland = false;
+let countryDescription =`${country} is located in ${continent} and about ${population} million people live here.`
+
+console.log(country);
+console.log(continent);
+console.log(population);
+console.log(countryDescription);
